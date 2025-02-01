845c8b98e6d56b946441a53b50611d5391630c5d
LukaszG92 <lukasz.gajewski15@gmail.com>
2025-02-01 11:26:53 +0100

    Changed filtering logic

--- scripts/filter_significant_urls.py
+++ scripts/filter_significant_urls.py
@@ -7,9 +7,8 @@ from custom_types.Platform import Platform
 def get_relevant_urls(url_stats: pd.DataFrame) -> pd.Series:
     counts = url_stats['count']
 
-    mean = counts.mean()
-    std = counts.std()
-    filtered = url_stats[counts > (mean + std)]
+    percentile_90 = counts.quantile(0.9)
+    filtered = url_stats[(counts > percentile_90) & (counts > 5)]
 
     return filtered['url']
 
@@ -20,17 +19,17 @@ def filter_texts_with_urls(df: pd.DataFrame, relevant_urls: pd.Series) -> pd.Dat
     return df[df['text'].str.contains(pattern, na=False, regex=True)]
 
 
-def filter_relevant_urls(platform: Platform) -> None:    
+def filter_relevant_urls(platform: Platform) -> None:
     df = pd.read_csv(f'./datasets/{platform}/url_stats.csv')
     relevant_urls = get_relevant_urls(df)
 
-    if os.path.exists(f"./datasets/{platform}/filtered_relevant_urls.csv"):
+    if os.path.exists(f"./datasets/{platform}/dataset_relevant_urls.csv"):
         print("Dataset already filtered for relevant urls")
         return
 
     print("Starting dataset filtering for relevant urls...")
-    df = pd.read_csv(f'./datasets/{platform}/filtered_urls.csv')
+    df = pd.read_csv(f'./datasets/{platform}/dataset_urls.csv')
     filtered_df = filter_texts_with_urls(df, relevant_urls)
 
-    filtered_df.to_csv(f'./datasets/{platform}/filtered_relevant_urls.csv', index=False)
+    filtered_df.to_csv(f'./datasets/{platform}/dataset_relevant_urls.csv', index=False)
     print(f"Correctly filtered {len(filtered_df)} contents with relevant URLs in dataset/{platform}/filtered_urls.csv")
